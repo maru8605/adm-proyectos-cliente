6bf700124d933faad956b5a5458f29522cea70cf
maru8605 <maru8605@hotmail.com>
2021-08-10 00:27:39 -0300

    componente nueva cuenta

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Link } from 'react-router-dom';
 
 const Login = () => {
 
-    const onChange = () => {
+    const [user, saveUser] = useState({
+        email:'',
+        password:''
+    }) 
+    
+    const {email, password} = user;
 
+    const onChange = e => {
+        saveUser({
+            ...user,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const onSubmit = e => {
+        e.preventDefault()
+
+        // validar campos
+
+        // pasa a action
     }
 
     return (
@@ -11,7 +30,7 @@ const Login = () => {
             <div className='contenedor-form sombra-dark'>
                 <h1>Inicia Sesión</h1>
 
-                <form>
+                <form onSubmit={onSubmit}>
                     <div className='campo-form'>
                         <label htmlFor='email'>Email</label>
                         <input
@@ -19,6 +38,7 @@ const Login = () => {
                             id='email'
                             name='email'
                             placeholder='Tu Email'
+                            value={email}
                             onChange={onChange}
                         />
                     </div>
@@ -30,6 +50,7 @@ const Login = () => {
                             id='password'
                             name='password'
                             placeholder='Tu Password'
+                            value={password}
                             onChange={onChange}
                         />
                     </div>
@@ -40,6 +61,10 @@ const Login = () => {
                           value='Iniciar Sesión'/>
                     </div>
                 </form>
+
+                <Link to='/new-account' className='enlace-cuenta'>
+                    Obtener Cuenta
+                </Link>
             </div>
         </div>
     )
--- src/components/auth/NewAccount.js
+++ src/components/auth/NewAccount.js
@@ -1,9 +1,96 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { Link } from 'react-router-dom';
 
 const NewAccount = () => {
+
+    const [user, saveUser] = useState({
+        nombre:'',
+        email:'',
+        password:'',
+        confirmar:''
+    }) 
+    
+    const {nombre, email, password, confirmar} = user;
+
+    const onChange = e => {
+        saveUser({
+            ...user,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const onSubmit = e => {
+        e.preventDefault()
+
+        // validar campos
+
+        // pasa a action
+    }
+
     return (
-        <div>
-            <h1>desde nueva cuenta</h1>
+        <div className='form-usuario'>
+            <div className='contenedor-form sombra-dark'>
+                <h1>Registrate</h1>
+
+                
+                <form onSubmit={onSubmit}>
+                    <div className='campo-form'>
+                        <label htmlFor='nombre'>Nombre</label>
+                        <input
+                            type='text'
+                            id='nombre'
+                            name='nombre'
+                            placeholder='Tu Nombre'
+                            value={nombre}
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className='campo-form'>
+                        <label htmlFor='email'>Email</label>
+                        <input
+                            type='email'
+                            id='email'
+                            name='email'
+                            placeholder='Tu Email'
+                            value={email}
+                            onChange={onChange}
+                        />
+                    </div>
+
+                    <div className='campo-form'>
+                        <label htmlFor='password'>Password</label>
+                        <input
+                            type='password'
+                            id='password'
+                            name='password'
+                            placeholder='Tu Password'
+                            value={password}
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className='campo-form'>
+                        <label htmlFor='confirmar'>Confirmar Password</label>
+                        <input
+                            type='password'
+                            id='confirmar'
+                            name='confirmar'
+                            placeholder='Confirma tu Password'
+                            value={confirmar}
+                            onChange={onChange}
+                        />
+                    </div>
+                    <div className='campo-form'>
+                        <input
+                         type='submit'
+                          className='btn btn-primario btn-block'
+                          value='Registrarme!'/>
+                    </div>
+                </form>
+
+                <Link to='/' className='enlace-cuenta'>
+                    Iniciar Sesión
+                </Link>
+            </div>
         </div>
     )
 }
