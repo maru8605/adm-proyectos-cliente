daf7b234379d1a60bb66db115e197e1e8f0f2c99
maru8605 <maru8605@hotmail.com>
2021-08-11 13:44:57 -0300

    funcion para llamar proyectos existentes

--- src/components/projects/ProjectsList.js
+++ src/components/projects/ProjectsList.js
@@ -1,17 +1,25 @@
-import React from 'react'
+import React, {useContext, useEffect} from 'react'
+import ProjectContext from '../../context/ProjectContext';
 import Project from './Project'
 
 const ProjectsList = () => {
+    // extraer proyectos de State Inicial
+    const projectContext = useContext(ProjectContext);
+    const {proyectos, getProjects} = projectContext;
 
-    const proyectos = [
-        {nombre: 'tienda virtual'},
-        {nombre: 'web personal'},
-        {nombre :'intranet'}
-    ]
+    useEffect(() => {
+       getProjects()
+    }, [])
+
+
+    // revisar si proyectos tiene contenido
+    if(proyectos.lenght === 0) return null
+    
     return (
         <ul className='listado-proyectos'>
            {proyectos.map(proyecto => (
                <Project
+                    id={proyecto.id}
                     proyecto={proyecto}
                />
            ))} 
--- src/context/ProjectState.js
+++ src/context/ProjectState.js
@@ -2,10 +2,20 @@
 import ProjectReducer from './ProjectReducer'
 import ProjectContext from './ProjectContext'
 import { useReducer } from 'react'
-import {FORM_PROJECT} from '../types'
+import {FORM_PROJECT, GET_PROJECTS} from '../types'
 
 const ProjectState = props => {
+
+    const projectos = [
+        { id: 1, nombre: 'tienda virtual' },
+        { id:2, nombre: 'web personal' },
+        { id:3, nombre :'intranet' },
+        { id:4, nombre : 'ejemplo'}
+    ]
+
     const initialState = {
+        
+        proyectos : [],
         formulario : false 
     }
 
@@ -19,12 +29,21 @@ const ProjectState = props => {
         })
     }
 
+    const getProjects = () => {
+        dispatch({
+            type: GET_PROJECTS,
+            payload: projectos
+        })
+    }
+
 
     return(
         <ProjectContext.Provider
             value={{
+                proyectos : state.proyectos,
                 formulario: state.formulario,
-                showForm
+                showForm,
+                getProjects
             }}
         >
             {props.children}
